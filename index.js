dbdb1cc1e07f2f18d269e74e98ad4a0b21dfc108
Leta Keane <letakeane@gmail.com>
2020-09-09 21:36:16 -0600

    Add initial greeting

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const beginButton = document.querySelector('.begin');
 const speechBubble = document.querySelector('.speech-bubble');
+const thoughtBubble = document.querySelector('.thought-bubble');
 const mouth = document.querySelector('.mouth');
 let words = document.querySelector('h2');
-
 let SpeechRecognition;
 let SpeechGrammarList;
 let SpeechRecognitionEvent;
 const grammar = '#JSGF V1.0; grammar moods; public <mood> = happy | sad | scared | tired | angry | confused ;'
 let recognition;
 let speechRecognitionList;
+// let synth;
+// let myVoices;
+// let myVoice;
+// let speech;
 
 window.onload = initializeSpeech;
+beginButton.addEventListener('click', () => speak(true));
 
 function initializeSpeech() {
   SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
@@ -20,12 +26,43 @@ function initializeSpeech() {
   speechRecognitionList.addFromString(grammar, 1);
   recognition.grammars = speechRecognitionList;
 
-  setTimeout(function() {
-    robotSpeakToggle();
-  }, 1600)
+  // synth = speechSynthesis;
+  // myVoices = synth.getVoices();
+  // myVoice = myVoices[10];
+
+  // beginButton.disabled = false;
 }
 
-function robotSpeakToggle() {
-  speechBubble.classList.toggle('hidden');
-  mouth.classList.toggle('speak');
+function robotStartSpeaking() {
+  speechBubble.classList.remove('hidden');
+  thoughtBubble.classList.add('hidden');
+  mouth.classList.add('speak');
 }
+
+function robotStopSpeaking() {
+  setTimeout(() => {
+    speechBubble.classList.add('hidden');
+    thoughtBubble.classList.remove('hidden');
+  }, 1000);
+  mouth.classList.remove('speak');
+}
+
+function speak(initial) {
+  if (initial) {
+    beginButton.classList.add('hidden');
+  }
+  
+  robotStartSpeaking();
+  synthVoice('Hello, friend! How are you feeling today?')
+}
+
+function synthVoice(text) {
+  const synth = window.speechSynthesis;
+  const thisVoice = synth.getVoices()[11];
+  const utterance = new SpeechSynthesisUtterance();
+  utterance.voice = thisVoice;
+  utterance.text = text;
+  synth.speak(utterance);
+  utterance.addEventListener('end', robotStopSpeaking);
+}
+
