31c97d34857478774302d903b46596006ceccc5e
Leta Keane <letakeane@gmail.com>
2020-09-09 20:06:03 -0600

    Create robot

---
+++ index.js
@@ -0,0 +1,29 @@
+const speechBubble = document.querySelector('.speech-bubble');
+const mouth = document.querySelector('.mouth');
+let SpeechRecognition;
+let SpeechGrammarList;
+let SpeechRecognitionEvent;
+const grammar = '#JSGF V1.0; grammar moods; public <mood> = happy | sad | scared | tired | angry | confused ;'
+let recognition;
+let speechRecognitionList;
+
+window.onload = initializeSpeech;
+
+function initializeSpeech() {
+  SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+  SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+  recognition = new SpeechRecognition();
+  speechRecognitionList = new SpeechGrammarList();
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+
+  setTimeout(function() {
+    robotSpeakToggle();
+  }, 1600)
+}
+
+function robotSpeakToggle() {
+  speechBubble.classList.toggle('hidden');
+  mouth.classList.toggle('speak');
+}
